a830ab9cdf6e47b8380aa85ece917dce5c8b2c7d
Renozixx <viniciusreno@uol.com.br>
2025-08-08 18:32:53 -0300

    Chaging to Go officially

---
+++ front-end/src/Views/Pages/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+       <header>
+        
+       </header>
+
+        <main>
+
+        </main>
+    </div>
+  )
+}
+
+export default Homepage
