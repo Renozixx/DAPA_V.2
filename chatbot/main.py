4428db3685e4acae532c00221d79ff659f03c324
Renozixx <viniciusreno@uol.com.br>
2025-08-24 17:20:55 -0300

    Creating the uvicorn setup of my chatbot module

---
+++ chatbot/main.py
@@ -0,0 +1,23 @@
+import uvicorn
+
+async def app(scope, receive, send):
+    assert scope['type'] == 'http'
+
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            (b'content-type', b'text/plain'),
+            (b'content-length', b'13'),
+        ],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello, world!',
+    })
+
+
+if __name__ == "__main__":
+    config = uvicorn.Config("main:app", port=8002, log_level="info")
+    server = uvicorn.Server(config)
+    server.run()
